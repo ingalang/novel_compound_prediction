d2ad4a188fffd3bcffca846bda96d6009baf5d04
ingalang <ingalung@hotmail.no>
2021-11-22 18:02:37 +0100

    fixed a bug

--- train_embeddings.py
+++ train_embeddings.py
@@ -15,11 +15,15 @@ class SentenceReader(object):
                 line = re.sub(regex, '', line).lower()
                 yield line.split()
 
-def train_model_on_year(top_dir, year, model_path=None):
+def train_model_on_year(top_dir, model_dir, year):
     print(f'Training model on {year} data')
+    previous_model_name = f'word2vec_{year - 1}.model'
+    previous_model_dir = os.path.join(model_dir, previous_model_name)
+    model_path = previous_model_dir if os.path.isfile(previous_model_dir) else None
     filepath = os.path.join(top_dir, f'COCA_{year}.txt')
     sentence_generator = SentenceReader(filepath)
     if model_path == None:
+        print('model path is none')
         model = Word2Vec(sentences=sentence_generator, vector_size=300, window=2, min_count=5, workers=3)
     else:
         print(f'loading model from: {model_path}')
@@ -28,11 +32,8 @@ def train_model_on_year(top_dir, year, model_path=None):
     return model
 
 def train_years(start_year, end_year, data_dir, model_dir):
-    previous_model_name = f'word2vec_{start_year - 1}.model'
-    previous_model_dir = os.path.join(model_dir, previous_model_name)
-    start_model_path = previous_model_dir if os.path.isfile(previous_model_dir) else None
     for i in range(start_year, end_year + 1):
-        model = train_model_on_year(top_dir=data_dir, year=i, model_path=start_model_path)
+        model = train_model_on_year(top_dir=data_dir, model_dir=model_dir, year=i)
         model.save(os.path.join(model_dir, f'word2vec_{i}.model'))
         del model
         print(f'Saved model trained on data from {i}')
