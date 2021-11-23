0959497dd70c6e8fa61fbd285cd815e5dfb84350
ingalang <ingalung@hotmail.no>
2021-11-23 11:20:50 +0100

    added dims parameter in train_embeddings and added classifier code in classifier notebook

--- train_embeddings.py
+++ train_embeddings.py
@@ -15,26 +15,26 @@ class SentenceReader(object):
                 line = re.sub(regex, '', line).lower()
                 yield line.split()
 
-def train_model_on_year(top_dir, model_dir, year):
+def train_model_on_year(top_dir, model_dir, year, dims):
     print(f'Training model on {year} data')
-    previous_model_name = f'word2vec_{year - 1}.model'
+    previous_model_name = f'word2vec_{year - 1}_{dims}.model'
     previous_model_dir = os.path.join(model_dir, previous_model_name)
     model_path = previous_model_dir if os.path.isfile(previous_model_dir) else None
     filepath = os.path.join(top_dir, f'COCA_{year}.txt')
     sentence_generator = SentenceReader(filepath)
     if model_path == None:
         print('model path is none')
-        model = Word2Vec(sentence_generator, vector_size=300, window=2, min_count=5, workers=3)
+        model = Word2Vec(sentence_generator, vector_size=dims, window=2, min_count=5, workers=3)
     else:
         print(f'loading model from: {model_path}')
         model = Word2Vec.load(model_path)
         model.train(sentence_generator, epochs=model.epochs, total_examples=model.corpus_count)
     return model
 
-def train_years(start_year, end_year, data_dir, model_dir):
+def train_years(start_year, end_year, data_dir, model_dir, dims):
     for i in range(start_year, end_year + 1):
-        model = train_model_on_year(top_dir=data_dir, model_dir=model_dir, year=i)
-        model.save(os.path.join(model_dir, f'word2vec_{i}.model'))
+        model = train_model_on_year(top_dir=data_dir, model_dir=model_dir, year=i, dims=dims)
+        model.save(os.path.join(model_dir, f'word2vec_{i}_{dims}.model'))
         del model
         print(f'Saved model trained on data from {i}')
 
@@ -48,12 +48,14 @@ def main():
     parser.add_argument('--data_name', default='train', required=False)
     parser.add_argument('--model_dir', default='test_COCA', required=False,
                         help='Path to directory where you want to save the word2vec models')
+    parser.add_argument('--dims', default=300, type=int,
+                        help='How many dimensions you want the word2vec vectors to be')
 
     args = parser.parse_args()
-    top_dir, start_year, end_year, data_name, model_dir = \
-        args.top_dir, args.start_year, args.end_year, args.data_name, args.model_dir
+    top_dir, start_year, end_year, data_name, model_dir, dims = \
+        args.top_dir, args.start_year, args.end_year, args.data_name, args.model_dir, args.dims
 
-    train_years(start_year=start_year, end_year=end_year, data_dir=top_dir, model_dir=model_dir)
+    train_years(start_year=start_year, end_year=end_year, data_dir=top_dir, model_dir=model_dir, dims=dims)
 
 if __name__ == '__main__':
     main()
