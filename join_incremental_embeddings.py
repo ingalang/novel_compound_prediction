da0165d0432b81cbb5e6691571491e5c4e890b04
ingalang <ingalung@hotmail.no>
2022-03-14 14:49:29 +0100

    jasonified np arrays

--- join_incremental_embeddings.py
+++ join_incremental_embeddings.py
@@ -18,7 +18,6 @@ def main():
     args = parser.parse_args()
     dims = args.dims
 
-
     with open(os.path.join(args.comp_file_dir, 'COCA_train_min3_no_doubles_filtered.txt'), 'r') as infile:
         train_compounds = [line.strip('\n\r') for line in infile]
 
@@ -30,14 +29,11 @@ def main():
     print(len(all_words))
     unique_constituents = np.unique(all_words)
 
-    embedding_df = pd.DataFrame()
-    embedding_df['word'] = unique_constituents
-
     def get_vec_for_word(word, model, dims):
         if word in model.wv:
-            return model.wv[word]
+            return model.wv[word].tolist()
         else:
-            return np.zeros(dims)
+            return np.zeros(dims).tolist()
 
     def get_full_vector(df, word):
         full_vec = df.loc[df['word'] == word]
@@ -47,25 +43,15 @@ def main():
 
     time_vec_dict = defaultdict(list)
     for year in range(args.start_year, args.end_year + 1):
-        word2vec_model = Word2Vec.load(os.path.join(args.model_dir, 'word2vec_{}_{}.model'.format(year, dims)))
+        print('Appending {} vectors...'.format(year))
+        word2vec_model = Word2Vec.load(os.path.join(args.model_dir, 'word2vec_{}_{}.model'.format(year, dims))) ##den riktige
         for word in unique_constituents:
             time_vec_dict[word].append(get_vec_for_word(word, word2vec_model, dims))
 
     with open('time_vecs.json', 'w') as outfile:
         json.dump(time_vec_dict, outfile)
 
-    new_df = pd.DataFrame.from_dict(time_vec_dict).T.reset_index(level=0)
-    print(new_df.head())
-
-    # test_df = pd.DataFrame()
-    # test_df['word'] = ['car', 'cat', 'mouse']
-    # print(test_df)
-    # test_dict = {'car' : [[1, 7, 18, 1], [2, 2, 2, 2]],
-    #              'cat' : [[3, 3, 3, 3], [4, 4, 4, 4]],
-    #              'mouse' : [[5, 5, 5, 5], [6, 6, 6, 6]]}
-    # new_df = pd.DataFrame.from_dict(test_dict).T.reset_index(level=0)
-    # new_df.columns = ['word']+list(range(1990,1991+1))
-    # print(new_df)
+    print('DONE')
 
 
 
