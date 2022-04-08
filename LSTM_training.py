7d3ad0848d59512118dd4d1621cc877d9da9cc31
ingalang <ingalung@hotmail.no>
2022-04-08 17:00:20 +0200

    fixes

--- LSTM_training.py
+++ LSTM_training.py
@@ -10,26 +10,48 @@ import json
 #from keras.utils import plot_model
 import argparse
 
+def train_LSTM_model(timesteps, input_vec_dims, input_sequence, num_samples, mode, epochs):
+    if mode == 'reconstruct':
+        # define model
+        model = Sequential()
+        model.add(LSTM(300, activation='relu', input_shape=(timesteps, input_vec_dims)))
+        model.add(RepeatVector(timesteps))
+        model.add(LSTM(300, activation='relu', return_sequences=True))
+        model.add(TimeDistributed(Dense(1)))
+        model.compile(optimizer='adam', loss='mse')
+        model.fit(input_sequence, input_sequence, epochs=epochs, verbose=2)
+        return model
+    elif mode == 'predict':
+        output_sequence = input_sequence[:, 1:, :]
+        n_out = timesteps - 1
+        model = Sequential()
+        model.add(LSTM(100, activation='relu', input_shape=(timesteps, input_vec_dims)))
+        model.add(RepeatVector(n_out))
+        model.add(LSTM(100, activation='relu', return_sequences=True))
+        model.add(TimeDistributed(Dense(1)))
+        model.compile(optimizer='adam', loss='mse')
+        model.fit(input_sequence, output_sequence, epochs=epochs, verbose=2)
+        return model
+    else:
+        raise ValueError('mode must be either reconstruct or predict!')
+
 
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--epochs', type=int, required=False, default=50)
+    parser.add_argument('--epochs', type=int, required=False, default=100)
 
     args = parser.parse_args()
     epochs = args.epochs
 
+    mode = 'predict'
+
     # define input sequence
 
-
-    ## test: la oss si du har vektorer med 5 dimensjoner. Du har 2 samples og 3 timesteps. Hvis alle vektorene for hver
-    # sample er lagra som én lang vektor, sånn :
-    # 'car' : [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3]
-    # 'boat': [4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6]
     with open('time_vecs.json', 'rb') as infile:
         time_vecs = json.load(infile)
 
-    time_steps = 20
+    timesteps = 20
     input_vec_dims = 300
     num_samples = len(time_vecs)
 
@@ -38,37 +60,30 @@ def main():
     print(len(sequence))
 
     # reshape input into [samples, timesteps, features]
-    sequence = sequence.reshape(num_samples, time_steps, input_vec_dims)
-    n_in = len(sequence)
+    sequence = sequence.reshape(num_samples, timesteps, input_vec_dims)
 
-    input_vectors = np.random.rand(num_samples, input_vec_dims * time_steps)
-    print(input_vectors.shape)
+    model = train_LSTM_model(timesteps=timesteps,
+                              input_vec_dims=input_vec_dims,
+                              input_sequence = sequence,
+                              num_samples = num_samples,
+                              mode = mode,
+                              epochs = epochs)
 
-    # sequence = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+    model.save('lstm_model_{}_{}_{}'.format(epochs, input_vec_dims, mode))
 
-
-    sequence = sequence.reshape((num_samples, time_steps, input_vec_dims))
-    print(sequence)
-    print(sequence.shape)
-    # define model
-    model = Sequential()
-    model.add(LSTM(300, activation='relu', input_shape=(time_steps, input_vec_dims)))
-    model.add(RepeatVector(time_steps))
-    model.add(LSTM(300, activation='relu', return_sequences=True))
-    model.add(TimeDistributed(Dense(1)))
-    model.compile(optimizer='adam', loss='mse')
-    # fit model
-    model.fit(sequence, sequence, epochs=epochs, verbose=1)
-    # plot_model(model, show_shapes=True, to_file='reconstruct_lstm_autoencoder.png')
-    model.save('lstm_model_{}_{}'.format(epochs, input_vec_dims))
-
-    #model = Model(inputs=model.inputs, outputs=model.layers[0].output)
+    model = Model(inputs=model.inputs, outputs=model.layers[0].output)
 
 
     # get the feature vector for the input sequence
-    #yhat = model.predict(sequence)
-    #print(yhat.shape)
-    #print(yhat)
+
+    test_sequence = np.array(time_vecs['car']).flatten()
+    print(test_sequence.shape)
+    test_sequence = test_sequence.reshape(1, timesteps, input_vec_dims)
+    print(test_sequence.shape)
+
+    yhat = model.predict(test_sequence)
+    print(yhat.shape)
+    print(yhat)
 
 
 if __name__ == '__main__':
