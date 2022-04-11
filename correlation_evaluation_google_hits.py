4212a75c141041e4b3f33f6967b20f8a95dacc53
ingalang <ingalung@hotmail.no>
2022-04-11 14:05:43 +0200

    added correlation eval script

---
+++ correlation_evaluation_google_hits.py
@@ -0,0 +1,44 @@
+import json
+import pandas as pd
+import numpy as np
+import random
+
+def main():
+    constituent = 'mod'
+    with open(f'results_300/cosine_novel/false_positives_cosine_{constituent}s_300hidden_50e.txt', 'r') as infile:
+        fps = [line.strip('\n\r') for line in infile]
+
+    with open(f'results_300/cosine_novel/false_negatives_cosine_{constituent}s_300hidden_50e.txt', 'r') as infile:
+        fns = [line.strip('\n\r') for line in infile]
+
+    with open(f'results_300/cosine_novel/true_positives_cosine_{constituent}s_300hidden_50e.txt', 'r') as infile:
+        tps = [line.strip('\n\r') for line in infile]
+
+    with open(f'results_300/cosine_novel/true_negatives_cosine_{constituent}s_300hidden_50e.txt', 'r') as infile:
+        tns = [line.strip('\n\r') for line in infile]
+
+    ## classified as positive: tps and fps
+    ## classified as negative: tns and fns
+
+    print(len(fps))
+    print(len(tps))
+    print(len(tns))
+    print(len(fns))
+
+    print()
+    print(len(tps)+len(fps))
+    print(len(tns)+len(fns))
+    print(len(np.concatenate((fns, tns), 0)))
+
+    print('random sample of positively classified compounds:')
+    random_pos = random.sample(tps+fps, 100)
+    for comp in random_pos:
+        print(comp)
+
+    print('random sample of negatively classified compounds:')
+    random_pos_n = random.sample(tns+fns, 100)
+    for comp in random_pos_n:
+        print(comp)
+
+if __name__ == '__main__':
+    main()
